fe3f8c4a1762e663402d0e2756611ebdaf143587
Komol Bunrueang <6810610037@cn102>
2025-09-04 13:14:57 +0700

    failed try again

--- ex_p54.py
+++ ex_p54.py
@@ -4,11 +4,19 @@ SERVICE_CHARGE = 24.62
 UNIT_RATE = 0
 if energy_unit >= 1 and energy_unit <= 150:
     UNIT_RATE = 3.2484
+    electric_cost = energy_unit * UNIT_RATE
 if energy_unit >= 150 and energy_unit <= 400:
+    energy_above_150 = energy_unit - 150
     UNIT_RATE = 4.2218
+    cost_above_150 = energy_above_150 * UNIT_RATE
+    electric_cost += cost_above_150
 if energy_unit > 400:
+    energy_above_400 = energy_unit - 400
     UNIT_RATE = 4.4217
-electric_cost = energy_unit * UNIT_RATE
+    cost_above_400 = energy_above_400 * UNIT_RATE
+    electric_cost += cost_above_400
+if energy_unit == 0:
+    electric_cost = energy_unit * UNIT_RATE
 VAT = (electric_cost + SERVICE_CHARGE) * 7 / 100
 total_cost = electric_cost + SERVICE_CHARGE + VAT
 print(f"Electricity cost = {electric_cost:,.2f} Baht")
